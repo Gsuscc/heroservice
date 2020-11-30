4b1428baa811a95fc232f1b36bbd2ce95ecedf2d
Gergely Molnar <molnar.gergo18@gmail.com>
2020-11-30 18:19:40 +0100

    search by name / full_name

--- heroservice/src/main/java/com/reactheroes/heroservice/repository/HeroRepository.java
+++ heroservice/src/main/java/com/reactheroes/heroservice/repository/HeroRepository.java
@@ -1,8 +1,19 @@
 package com.reactheroes.heroservice.repository;
 
 import com.reactheroes.heroservice.entity.Hero;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface HeroRepository extends JpaRepository<Hero, Long> {
 
+    @Query("SELECT h FROM Hero h " +
+            "WHERE lower(h.biography.fullName) " +
+            "LIKE lower(concat('%', :value,'%')) " +
+            "OR lower(h.name) " +
+            "LIKE lower(concat('%', :value,'%'))")
+    List<Hero> getByAnything(String value, Pageable page);
 }
