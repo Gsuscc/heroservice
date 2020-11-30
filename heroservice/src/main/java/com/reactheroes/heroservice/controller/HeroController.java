e3c33944ca9845ac6e5b52688ea49eafec505c83
Gergely Molnar <molnar.gergo18@gmail.com>
2020-11-30 18:19:40 +0100

    search by name / full_name

--- heroservice/src/main/java/com/reactheroes/heroservice/controller/HeroController.java
+++ heroservice/src/main/java/com/reactheroes/heroservice/controller/HeroController.java
@@ -3,7 +3,9 @@ package com.reactheroes.heroservice.controller;
 import com.reactheroes.heroservice.entity.Hero;
 import com.reactheroes.heroservice.repository.HeroRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.convert.ReadingConverter;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -20,15 +22,21 @@ public class HeroController {
     }
 
 
-
-    @GetMapping("/get/{id}")
-    public ResponseEntity<Hero> getHero(@PathVariable Long id){
+    @GetMapping("/hero")
+    public ResponseEntity<Hero> getHero(@RequestParam Long id){
         return ResponseEntity.ok(heroRepository.findById(id).get());
     }
 
-    @GetMapping("heroes")
+    @GetMapping("/heroes")
     public ResponseEntity<Object> getHeroesPerPage(@RequestParam Integer page){
         return ResponseEntity.ok(heroRepository.findAll(PageRequest.of(page, 10)));
+    }
+
+
+    @GetMapping("/search")
+    public ResponseEntity<Object> getHeroByName(@RequestParam String value){
+//        return null;
+        return ResponseEntity.ok(heroRepository.getByName(value, PageRequest.of(0,2)));
     }
 
 
