1ba75b0c8fd326ace7eaab7f70fdf1b4cdefa765
attila-h <hauth.attila@gmail.com>
2020-12-01 12:44:44 +0100

    configure security for proxy

---
+++ zuul/src/main/java/com/reactheroes/zuul/security/JwtTokenServices.java
@@ -0,0 +1,82 @@
+package com.reactheroes.zuul.security;
+
+import io.jsonwebtoken.*;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import javax.servlet.http.HttpServletRequest;
+import java.util.Base64;
+import java.util.Date;
+
+@Component
+@Slf4j
+public class JwtTokenServices {
+
+    @Value("${security.jwt.token.secret-key:secret}")
+    private String secretKey = "secret";
+
+    @Value("${security.jwt.token.expire-length:3600000}")
+    private long validityInMilliseconds = 36000000; // 10 hours
+
+
+    @PostConstruct
+    protected void init() {
+        secretKey = Base64.getEncoder().encodeToString(secretKey.getBytes());
+    }
+
+    // Creates a JWT token
+//    public String createToken(String username) {
+//        // Add a custom field to the token
+//        Claims claims = Jwts.claims().setSubject(username);
+//
+//        Date now = new Date();
+//        Date validity = new Date(now.getTime() + validityInMilliseconds);
+//
+//        return Jwts.builder()
+//                .setClaims(claims)
+//                .setIssuedAt(now)
+//                .setExpiration(validity)
+//                .signWith(SignatureAlgorithm.HS256, secretKey)
+//                .compact();
+//    }
+
+    String getTokenFromRequest(HttpServletRequest req) {
+        String bearerToken = req.getHeader("Authorization");
+        if (bearerToken != null && bearerToken.startsWith("Bearer ")) {
+            return bearerToken.substring(7);
+        }
+        return null;
+    }
+
+    // checks if the token is valid and not expired.
+    boolean validateToken(String token) {
+        try {
+            Jws<Claims> claims = Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token);
+            if (claims.getBody().getExpiration().before(new Date())) {
+                return false;
+            }
+            return true;
+        } catch (JwtException | IllegalArgumentException e) {
+            log.debug("JWT token invalid " + e);
+        }
+        return false;
+    }
+
+    /**
+     * Parses the username and roles from the token. Since the token is signed we can be sure its valid information.
+     * Note that it does not make a DB call to be super fast!
+     * This could result in returning false data (e.g. the user was deleted, but their token has not expired yet)
+     * To prevent errors because of this make sure to check the user in the database for more important calls!
+     */
+    Authentication parseUserFromTokenInfo(String token) throws UsernameNotFoundException {
+        Claims body = Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token).getBody();
+        String username = body.getSubject();
+
+        return new UsernamePasswordAuthenticationToken(username, "");
+    }
+}
---
+++ zuul/src/main/java/com/reactheroes/zuul/security/SecurityConfig.java
@@ -0,0 +1,46 @@
+package com.reactheroes.zuul.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    private final JwtTokenServices jwtTokenServices;
+
+    public SecurityConfig(JwtTokenServices jwtTokenServices) {
+        this.jwtTokenServices = jwtTokenServices;
+    }
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.
+                httpBasic().disable()
+                .cors().disable()
+                .csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeRequests()
+                .antMatchers(HttpMethod.POST, "/api/auth/**").permitAll()
+                .antMatchers(HttpMethod.GET, "/api/heroes").permitAll()
+                .antMatchers(HttpMethod.GET, "/api/hero").permitAll()
+                .antMatchers(HttpMethod.GET, "/api/search").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .addFilterBefore(new JwtTokenFilter(jwtTokenServices), UsernamePasswordAuthenticationFilter.class);
+    }
+
+}
+
