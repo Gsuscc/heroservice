1c70182fbc3b6fb47d01707de56387b2c1c38caa
attila-h <hauth.attila@gmail.com>
2020-12-17 08:03:27 +0100

    add missing controller annotation

--- userservice/src/main/java/com/reactheroes/userservice/controller/HeroCardController.java
+++ userservice/src/main/java/com/reactheroes/userservice/controller/HeroCardController.java
@@ -9,10 +9,12 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.NoSuchElementException;
 
+@RestController
 public class HeroCardController {
 
     private final HeroCardService heroCardService;
