ae544a65c48eb9bab66feb75119ab6300807bf70
Gergely Molnar <molnar.gergo18@gmail.com>
2021-01-06 13:58:06 +0100

    get myarmy properly

--- userservice/src/main/java/com/reactheroes/userservice/controller/UserArmyController.java
+++ userservice/src/main/java/com/reactheroes/userservice/controller/UserArmyController.java
@@ -2,8 +2,12 @@ package com.reactheroes.userservice.controller;
 
 import com.reactheroes.userservice.dao.interfaces.HeroCardDao;
 import com.reactheroes.userservice.dao.interfaces.UserArmyDao;
+import com.reactheroes.userservice.entity.HeroCard;
+import com.reactheroes.userservice.entity.UserArmy;
 import com.reactheroes.userservice.model.Army;
 import com.reactheroes.userservice.security.JwtTokenServices;
+import com.reactheroes.userservice.service.HeroCardGenerator;
+import com.reactheroes.userservice.service.HeroCardService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,6 +17,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.List;
 
 @RestController
 public class UserArmyController {
@@ -20,18 +25,29 @@ public class UserArmyController {
     private final UserArmyDao userArmyDao;
     private final HeroCardDao heroCardDao;
     private final JwtTokenServices jwtTokenServices;
+    private final HeroCardService heroCardService;
+    private final HeroCardGenerator heroCardGenerator;
 
     @Autowired
-    public UserArmyController(UserArmyDao userArmyDao, HeroCardDao heroCardDao, JwtTokenServices jwtTokenServices) {
+    public UserArmyController(UserArmyDao userArmyDao, HeroCardDao heroCardDao, JwtTokenServices jwtTokenServices, HeroCardService heroCardService, HeroCardGenerator heroCardGenerator) {
         this.userArmyDao = userArmyDao;
         this.heroCardDao = heroCardDao;
         this.jwtTokenServices = jwtTokenServices;
+        this.heroCardService = heroCardService;
+        this.heroCardGenerator = heroCardGenerator;
     }
 
     @GetMapping("/myarmy")
     private ResponseEntity<?> getMyArmy(HttpServletRequest httpServletRequest) {
         String email = jwtTokenServices.getEmailFromToken(httpServletRequest);
-        return ResponseEntity.ok(userArmyDao.getUserArmyByEmail(email));
+        UserArmy userArmy = userArmyDao.getUserArmyByEmail(email);
+        List<HeroCard> army = List.of(heroCardService.getCardByUniqueId(userArmy.getCard1(), email),
+                heroCardService.getCardByUniqueId(userArmy.getCard2(), email),
+                heroCardService.getCardByUniqueId(userArmy.getCard3(), email),
+                heroCardService.getCardByUniqueId(userArmy.getCard4(), email),
+                heroCardService.getCardByUniqueId(userArmy.getCard5(), email));
+        heroCardGenerator.generateMultipleCards(army);
+        return ResponseEntity.ok(army);
     }
 
     @PostMapping("/setarmy")
